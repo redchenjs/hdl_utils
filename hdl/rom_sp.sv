2ab80108d8f980bfbbaaeecf0cae6d81abdf9b3e
Jack Chen <redchenjs@live.com>
2023-05-11 00:13:42 +0800

    hdl: add rom_sp module

---
+++ hdl/rom_sp.sv
@@ -0,0 +1,41 @@
+/*
+ * rom_sp.sv
+ *
+ *  Created on: 2023-05-10 23:47
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+`timescale 1 ns / 1 ps
+
+module rom_sp #(
+    parameter PATH = "rom_init.txt",
+    parameter WIDTH = 8,
+    parameter DEPTH = 8,
+    parameter logic OUT_REG = 1'b1
+) (
+    input logic rd_clk_i,
+
+    input  logic                     rd_en_i,
+    input  logic [$clog2(DEPTH)-1:0] rd_addr_i,
+    output logic         [WIDTH-1:0] rd_data_o
+);
+
+logic [WIDTH-1:0] rom[DEPTH];
+
+initial begin
+    $readmemh(PATH, rom);
+end
+
+generate
+    if (!OUT_REG) begin
+        assign rd_data_o = rom[rd_addr_i];
+    end else begin
+        always_ff @(posedge rd_clk_i) begin
+            if (rd_en_i) begin
+                rd_data_o <= rom[rd_addr_i];
+            end
+        end
+    end
+endgenerate
+
+endmodule
---
+++ sim/test_rom_sp.sv
@@ -0,0 +1,59 @@
+/*
+ * test_rom_sp.sv
+ *
+ *  Created on: 2023-05-11 05:31
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+`timescale 1 ns / 1 ps
+
+module test_rom_sp;
+
+parameter PATH = "rom_init.txt";
+parameter WIDTH = 32;
+parameter DEPTH = 65536;
+parameter logic OUT_REG = 1'b1;
+
+logic rd_clk_i;
+
+logic                     rd_en_i;
+logic [$clog2(DEPTH)-1:0] rd_addr_i;
+logic         [WIDTH-1:0] rd_data_o;
+
+rom_sp #(
+    .PATH(PATH),
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH),
+    .OUT_REG(OUT_REG)
+) rom_sp (
+    .rd_clk_i(rd_clk_i),
+
+    .rd_en_i(rd_en_i),
+    .rd_addr_i(rd_addr_i),
+    .rd_data_o(rd_data_o)
+);
+
+initial begin
+    rd_clk_i = 'b0;
+
+    rd_en_i   = 'b0;
+    rd_addr_i = 'b0;
+end
+
+always begin
+    #2.5 rd_clk_i = ~rd_clk_i;
+end
+
+always begin
+    #5 rd_en_i = 'b1;
+
+    for (int i = 0; i < 65536; i++) begin
+        #5 rd_addr_i = rd_addr_i + 'b1;
+    end
+
+    #5 rd_en_i = 1'b0;
+
+    #25 $finish;
+end
+
+endmodule
