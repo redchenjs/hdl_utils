828465493bcb18b5863a7ab5beff5b69cd76cde4
Jack Chen <redchenjs@live.com>
2023-11-13 01:46:35 +0800

    hdl: improve tmds_encoder performance

--- hdl/tmds/tmds_encoder.sv
+++ hdl/tmds/tmds_encoder.sv
@@ -37,6 +37,8 @@ function logic [3:0] N_1(
     return x_1[0] + x_1[1];
 endfunction
 
+logic de, c1, c0;
+
 logic        [3:0] n_0;
 logic        [3:0] n_1;
 
@@ -47,6 +49,7 @@ logic signed [4:0] c_t;
 logic signed [4:0] c_n;
 
 logic        [8:0] q_m;
+logic        [8:0] q_r;
 logic        [9:0] q_n;
 
 always_comb begin
@@ -72,16 +75,16 @@ always_comb begin
         q_m[8] = 1'b1;
     end
 
-    n_0 = N_1(~q_m[7:0]);
-    n_1 = N_1( q_m[7:0]);
+    n_0 = N_1(~q_r[7:0]);
+    n_1 = N_1( q_r[7:0]);
 
     m_0 = c_t - (n_1 - n_0);
     m_1 = c_t + (n_1 - n_0);
 
-    if (!de_i) begin
+    if (!de) begin
         c_n = 'b0;
 
-        case ({c1_i, c0_i})
+        case ({c1, c0})
             2'b00: q_n = 10'b11_0101_0100;
             2'b01: q_n = 10'b00_1010_1011;
             2'b10: q_n = 10'b01_0101_0100;
@@ -89,19 +92,19 @@ always_comb begin
         endcase
     end else begin
         if ((c_t == 0) | (n_1 == n_0)) begin
-            c_n = q_m[8] ? m_1 : m_0;
+            c_n = q_r[8] ? m_1 : m_0;
 
-            q_n = {~q_m[8], q_m[8], (q_m[8] ? q_m[7:0] : ~q_m[7:0])};
+            q_n = {~q_r[8], q_r[8], (q_r[8] ? q_r[7:0] : ~q_r[7:0])};
         end else begin
             if (((c_t > 0) & (n_1 > n_0)) |
                 ((c_t < 0) & (n_1 < n_0))) begin
-                c_n = m_0 + { q_m[8], 1'b0};
+                c_n = m_0 + { q_r[8], 1'b0};
 
-                q_n = {1'b1, q_m[8], ~q_m[7:0]};
+                q_n = {1'b1, q_r[8], ~q_r[7:0]};
             end else begin
-                c_n = m_1 - {~q_m[8], 1'b0};
+                c_n = m_1 - {~q_r[8], 1'b0};
 
-                q_n = {1'b0, q_m[8],  q_m[7:0]};
+                q_n = {1'b0, q_r[8],  q_r[7:0]};
             end
         end
     end
@@ -110,10 +113,22 @@ end
 always_ff @(posedge clk_i or negedge rst_n_i)
 begin
     if (!rst_n_i) begin
+        de <= 'b0;
+        c1 <= 'b0;
+        c0 <= 'b0;
+
         c_t <= 'b0;
+
+        q_r <= 'b0;
         q_o <= 'b0;
     end else begin
+        de <= de_i;
+        c1 <= c1_i;
+        c0 <= c0_i;
+
         c_t <= c_n;
+
+        q_r <= q_m;
         q_o <= q_n;
     end
 end
