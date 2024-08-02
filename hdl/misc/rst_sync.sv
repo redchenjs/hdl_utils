f5e2b9ddce370e250f23c40f6b452bcae6fab267
Jack Chen <redchenjs@live.com>
2024-08-02 10:00:28 +0800

    hdl: update rst_sync module

--- hdl/misc/rst_sync.sv
+++ hdl/misc/rst_sync.sv
@@ -7,24 +7,26 @@
 
 `timescale 1 ns / 1 ps
 
-module rst_sync(
+module rst_sync #(
+    parameter int S_STAGE = 2
+) (
     input logic clk_i,
 
     input  logic rst_n_i,
     output logic rst_n_o
 );
 
-logic rst_n_t;
+// Data Sync
+data_sync #(
+    .S_STAGE(S_STAGE),
+    .I_VALUE(0),
+    .D_WIDTH(1)
+) u_data_sync (
+    .clk_i(clk_i),
+    .rst_n_i(rst_n_i),
 
-always_ff @(posedge clk_i or negedge rst_n_i)
-begin
-    if (!rst_n_i) begin
-        rst_n_t <= 'b0;
-        rst_n_o <= 'b0;
-    end else begin
-        rst_n_t <= 'b1;
-        rst_n_o <= rst_n_t;
-    end
-end
+    .data_i(1'b1),
+    .data_o(rst_n_o)
+)
 
 endmodule
